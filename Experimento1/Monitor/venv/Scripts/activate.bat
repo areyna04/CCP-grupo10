4d4daf023a180f9102b566db8d188dd256268f5b
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-02-24 01:19:35 -0600

    Arreglar issues venv

--- Experimento1/Monitor/venv/Scripts/activate.bat
+++ Experimento1/Monitor/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\artur\Desktop\CCP-grupo10\Experimento1\venv
+set VIRTUAL_ENV=C:\Users\artur\Desktop\CCP-grupo10\Experimento1\Monitor\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
