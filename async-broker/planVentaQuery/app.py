2c0acb2e569c07bb3a5024ffe6dbf56413e76d69
Mario Gomez <mariorobingomez@gmail.com>
2023-02-26 02:07:07 -0500

    fix: planVentaQuery

--- async-broker/planVentaQuery/app.py
+++ async-broker/planVentaQuery/app.py
@@ -4,32 +4,16 @@ from celery import Celery
 
 # Definicion Ambiente Microservicio
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/productos.db'
+
 api = Api(app)
-
-#se instancia para cuando colocar tareas en cola para workers
-celery_app = Celery(__name__, broker='redis://localhost:6379/0')
-
-# función para enviar a cola redis como parametro
-@celery_app.task(name='plan_venta')
-def get_plan_venta(*args):
-    pass
-
 # Recurso HTTP: /planventa/cliente
 class PlanVenta(Resource):
-    def post(self):
-        cliente = request.json["cliente"]
-        if cliente:
-            args = (cliente,)
-            #--Tarea Celery colocada en cola redis
-            get_plan_venta.apply_async(args)
-            return {'Tarea: iniciada'}, 200
-        else:
-            return {'no existe user'}, 404
-
+    def get(self):
+        pass
+ 
 #Urls Servicio
-api.add_resource(PlanVenta, '/planes/<string:cliente>')
+api.add_resource(PlanVenta, '/planes/')
 
-#Microservicio 2 PlanVentaProcessor  port = 8000
+#Microservicio 2 PlanVentaProcessor  port = 5003
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)   
+    app.run(debug=True, host='0.0.0.0', port=5003)   
