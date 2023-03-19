d694f92ad3043fb60428a94f2e8f6c92f069c415
lwilches <lwilches@segurosmundial.com.co>
2023-03-18 22:00:14 -0500

    ajuste token

--- Experimento2/APIGateway/app.py
+++ Experimento2/APIGateway/app.py
@@ -9,13 +9,10 @@ def get_status():
 
 @app.route('/login')
 def get_user_and_password():
-    #user = request.args.get('user')
-    #password = request.args.get('password')
+    
     auth_header   = request.authorization
     user = auth_header.username 
     password = auth_header.password
-    
-    ##params = {'user': user, 'password': password}
     
     
     
@@ -30,11 +27,16 @@ def get_user_and_password():
 @app.route('/planDeVenta')
 def get_plan_de_venta():
     id = request.args.get('id')
-    token = request.args.get('token')
-    user = request.args.get('user')
-    params = {'id': id, 'token': token, 'user': user}
+    #token = request.args.get('token')
+    token = request.headers['authorization']
+    user  = request.headers['user']
+    
+    #print(auth_header)
+    #user = request.args.get('user')
+    params = {'id': id}
+    headers = {'authorization': token,  'user': user}
     url = 'http://127.0.0.1:8002/planDeVenta'
-    response = requests.get(url, params=params)
+    response = requests.get(url,  params=params,  headers= headers)
     data = response.json()
     if response.status_code == 200:
         return data
